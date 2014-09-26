7cef2e3d6d30be28d660eedbb2ed55f79490cf18
Vladimir Ovsyukov <VovanSuper@users.noreply.github.com>
2014-09-26 16:32:23 +0400

    Update js_multi_inheritance.js

--- lame-js-protoInher/js_multi_inheritance.js
+++ lame-js-protoInher/js_multi_inheritance.js
@@ -1,4 +1,4 @@
-// ака, прототинпое мултинаследование
+// sketch of idea of mutitple proto inheritance . Compiled it using tsc node compiler
 var __extends = this.__extends || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
